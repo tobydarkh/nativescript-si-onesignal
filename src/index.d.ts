1a4fb83685226732bb728cb1f8b4ebd8d564bc04
Tobias Schanze <tobias.schanze@studiointeractive.de>
2022-04-20 14:37:43 +0200

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './nativescript-si-onesignal.common';
+export declare class NativescriptSiOnesignal extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
